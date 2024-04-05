4828b5673ce802af35c67c248f2637ca270c9800
Jacobo G. Lopez <jaco401@hotmail.com>
2024-04-05 09:22:36 -0500

    Se realiza la interacción de los diferentes elementos de la UI

---
+++ Assets/UI/UI.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    // Se definen cada uno de los menus del juego
+    public GameObject mainMenu;
+    public GameObject mainMenuBox;
+    public GameObject optionsMenu;
+    public GameObject inGameUI;
+    public GameObject pauseMenu;
+    public GameObject pauseMenuBox;
+
+    public bool gameStarted = false;
+
+    // ------------- MENÚ PRINCIPAL ---------------
+
+    // Boton START
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Level 1"); // Cargar el primer nivel desde el menú de inicio
+        mainMenu.SetActive(false); // Ocultar menu principal
+        inGameUI.SetActive(true); // Se muestra la UI del juego
+
+        gameStarted = true;
+    }
+    
+    // Boton OPTIONS
+    public void OptionsMenu()
+    {
+        if(gameStarted == false)
+        {
+            mainMenuBox.SetActive(false); // Se oculta el menu box
+            optionsMenu.SetActive(true); // Se muestra el menú de opciones
+        } 
+        else if(gameStarted == true) 
+        {
+            pauseMenuBox.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+        
+    }
+
+     // Boton BACK del menú de opciones
+    public void OptionsBackButton()
+    {
+        if(gameStarted == false)
+        {
+            optionsMenu.SetActive(false);
+            mainMenuBox.SetActive(true);
+        }
+        else if(gameStarted == true) 
+        {
+            pauseMenuBox.SetActive(true);
+            optionsMenu.SetActive(false);
+        }
+        
+    }
+
+    // ------------- UI EN EL JUEGO ---------------
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0; // Se pausa el tiempo de ejecución en el juego
+        pauseMenu.SetActive(true); // Se muestra el menú de pausa
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1; // Se resume el tiempo de ejecución del juego
+        pauseMenu.SetActive(false); // Se oculta el menú de pausa
+    }
+
+    public void QuitGame()
+    {
+        
+    }
+
+}
